ce58dc3f9ec8fcced083296f09e27531c13f65f0
Dustin Conway <dustinconway@gmail.com>
2020-09-13 19:44:14 -0500

    Project update

--- js/script.js
+++ js/script.js
@@ -10,19 +10,41 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+var quotes = [
+  {
+     text: '“Thousands of tired, nerve-shaken, over-civilized people are beginning to find out that going to the mountains is going home; that wildness is a necessity” ',
+     source: 'John Muir'
+  },
+  {
+    text: '“We need the tonic of wildness...At the same time that we are earnest to explore and learn all things, we require that all things be mysterious and unexplorable, that land and sea be indefinitely wild, unsurveyed and unfathomed by us because unfathomable. We can never have enough of nature.” ',
+    source: 'Henry David thoreau'
+  },  {
+    text: '“To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.”',
+    source: 'Ralph Waldo Emerson'
+  },  {
+    text: '“The proper function of man is to live, not to exist. I shall not waste my days in trying to prolong them. I shall use my time.” ',
+    source: 'Jack London'
+  },  {
+    text: '“The proper function of man is to live, not to exist. I shall not waste my days in trying to prolong them. I shall use my time."',
+    source: 'Robert Louis Stevenson'
+  },
+];
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  let randomQuote = Math.floor(Math.random() * quotes.length);
+  return quotes[randomQuote];
+}
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
 
+}
 
 
 /***
